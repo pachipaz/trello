8c41af14b4130de699b1d48b085d1c6a896e0013
pachipaz <raquelpaz.gutierrez@gmail.com>
2016-11-09 09:43:10 -0300

    parte uno

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function entrada (){
+	//contenedor nuevas entradas
+	//var agregar=document.getElementById('boton');
+	//agregar.classList.toggle('hidden')
+	// agregar nuevas tareas 
+	//var agragarEntradas=document.createElement('div'); 
+// elementos para ingresar listas 
+//var cajaOk=document.createElement('div');
+//var cajaNew=document.createElement('tarea');
+//var guardar=document.createElement('boton');
+//var texto=document.createTextNode('cajaNew');
